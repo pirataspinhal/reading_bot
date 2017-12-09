d0edb3900ab400c2d857b5018efec3c8a6998fe9
Gustavo Aguiar <gugahoa@gmail.com>
2017-12-08 23:33:42 -0200

    telegram_bot: reset state after one week, update offset when new msg arrives

--- apps/telegram_bot/lib/telegram_bot.ex
+++ apps/telegram_bot/lib/telegram_bot.ex
@@ -1,21 +1,26 @@
 defmodule TelegramBot do
+  require Logger
   use GenServer
 
   def start_link() do
-    GenServer.start_link(__MODULE__, [], 0)
+    GenServer.start_link(__MODULE__, {nil, 0})
   end
 
   def init(state) do
     fetch_updates()
-    :ok
+    {:ok, state}
   end
 
-  def handle_info(:get_updates, state) do
+  def handle_info(:get_updates, {timer_ref, offset} = state) do
     fetch_updates()
     state =
-      case Nadia.get_updates(allowed_updates: ["message"], offset: state) do
+      case Nadia.get_updates(allowed_updates: ["message"], offset: offset) do
+        {:ok, []} ->
+          state
         {:ok, telegram_updates} ->
-          save_to_db(telegram_updates)
+          timer_ref = reset_state(timer_ref)
+          Logger.debug "Received #{length(telegram_updates)} updates"
+          {timer_ref, save_to_db(telegram_updates) + 1}
         _ ->
           state
       end
@@ -27,7 +32,19 @@ defmodule TelegramBot do
     Process.send_after(self(), :get_updates, 200)
   end
 
+  defp save_to_db([last]) do
+    last.update_id
+  end
   defp save_to_db([first | rest]) do
+    save_to_db(rest)
   end
   defp save_to_db(_), do: nil
+
+  defp reset_state(nil) do
+    Process.send_after(self(), :reset_state, 7 * 24 * 60 * 60 * 1000)
+  end
+  defp reset_state(timer_ref) do
+    Process.cancel_timer(timer_ref)
+    Process.send_after(self(), :reset_state, 7 * 24 * 60 * 60 * 1000)
+  end
 end
